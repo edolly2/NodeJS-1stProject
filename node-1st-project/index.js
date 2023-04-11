46abca1afc84a20a55114553cd81cd3a27d22381
dollyllama <edolly2@protonmail.com>
2023-04-11 02:59:58 -0500

    ""

---
+++ node-1st-project/index.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('./index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/about') {
+    fs.readFile('./about.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/contact-me') {
+    fs.readFile('./contact-me.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page Not Found');
+  }
+});
+
+server.listen(8000, () => {
+  console.log('Server started and listening on port: 8000');
+});
